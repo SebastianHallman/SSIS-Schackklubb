4cbd20418ab9ac2fe2bebb9d44d79e50a86fa302
Sebastian <sebastian_hellman@live.se>
2019-11-14 18:33:32 +0100

    f

---
+++ schack.ssis.nu/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Logga in</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="" type="text/css" />
+
+</head>
+
+<body>
+<div class="container login"
+<form method="POST" action="">
+	<p>Användarnamn</p>
+	<input type="text" name="username"/>
+	<p>Lösenord</p>
+	<input type="password" name="userpass"/>
+	<input type="submit" value="Logga in" class="btn btn-primary"/>
+</form>
+</div>
+</body>
+</html>
--- schack.ssis.nu/members.php
+++ schack.ssis.nu/members.php
@@ -9,54 +9,44 @@
 	
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">SSIS Schackklubb</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Hem <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link active" href="#">Medlemmar</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link" href="#">.Turneringar</a>
       </li>
     </ul>
   </div>
 </nav>
 <div class="jumbotron jumbotron-fluid">
   <div class="container text-center">
-    <h1 class="display-4">Schack på SSIS</h1>
-    <p class="lead">Varje fredag håller Sebastian Hällman (17seha) schackturneringar i klassrummet schack klockan 16:00. Kom och njut av hjärngympa och skitsnack!</p>
+    <h1 class="display-4">Medlemmar</h1>
+    <p class="lead">Se vilka som är med i skolans schackklubb!</p>
   </div>
 </div>
+<div class="text-center container">
+	<table class="table">
+		<thead>
+			<tr>
+				<th scope="col">Skol-alias</th>
+				<th scope="col">Lichess-alias</th>
+			</tr>
+		</thead>
+		<tr>
+			<td>17seha</td>
+			<td>D1sabl3d</td>
+		</tr>
+</div>
 
-<div class="container text-center">
-	<h2>Var inte rädd att delta</h2>
-	<div class="pt-5 row">
-		<div class="col-4">
-			<h4>Erfarenhet spelar ingen roll</h4>
-			<p>Vare sig du är nybörjare eller veteran så anpassar vi oss efter allas behov.</p>
-			
-		</div>
-		<div class="col-4">
-			<h4>Humoristiskt umgänge</h4>
-			<p>Vi är väldigt sociala när vi spelar. Tro inte att vi är tysta! Det är ju fredag trotsallt</p>
-		</div>
-		<div class="col-4">
-			<h4>Flexibel tid</h4>
-			<p>Vi har sagt att vi börjar 16, men om klassrummet är ledigt tidigare kan man börja tidigare. Vi håller på tills vi inte orkar mer eller tills skolan stänger.</p>
-		</div>
-	</div>
-</div>
-<div class="container text-center pt-4">
-	<h2>Gå med i våran Discord!</h2>
-	<p>Gå med i vår Discordserver för information och spontana turneringar under helger.</p>
-	<a href="https://discord.gg/4HucSGG"><button class="btn btn-primary">Gå med nu!</button></a>
-</div>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
